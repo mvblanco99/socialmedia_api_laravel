cfec3f24c1f182ec4677ad862b139722b1532224
mvblanco99 <manuelvalentin1999@gmail.com>
2024-05-01 17:40:24 -0400

    Modificamos el paramentro Post de la funcion move por el parametro user

--- app/Services/ImageServices.php
+++ app/Services/ImageServices.php
@@ -5,35 +5,35 @@ namespace App\Services;
 use App\Http\Requests\ImageRequest;
 use App\Models\Image;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
 class ImageServices
 {
 
-  public function move(UploadedFile $image, Post $post)
+  public function move(UploadedFile $image, string $user_id)
   {
+    $user = User::find($user_id);
     $filename = time() . '.' . $image->extension();
-    $image->storeAs('public/images/'. $post->user->name . "" . $post->user_id .   '/',$filename);
+    $image->storeAs('public/Images/'. $user->name . "" . $user->id .   '/',$filename);
     return $filename;
   }
 
   public function createImage(string $filename, Post $post)
   {
-    $arrayData = [$filename, $post->id];
     $imageRequest = new ImageRequest();
-    $authorize = $imageRequest->merge($arrayData);
-
-    $image = null;
+    $authorize = $imageRequest->merge([$filename,$post->id]);
+    $newImage = null;
 
     if($authorize){
-      $image = Image::create([
+      $newImage = Image::create([
         'post_id' => $post->id,
-        'url' => url(Storage::url('public/images/' . "" .$post->user->name . "" . $post->user_id . "/". $filename ))
+        'url' => 
+          url(Storage::url('public/Images/' . "" .$post->user->name . "" . $post->user_id . "/". $filename ))
       ]);
     }
-
-    return $image;
+    return $newImage;
   }
 
 }
